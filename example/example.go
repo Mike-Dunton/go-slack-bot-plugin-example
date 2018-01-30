e9ecf9da406e35c3aff2c1efab842c10c317fe27
Michael Dunton <mdunton@gannett.com>
2018-01-30 02:10:47 -0500

    Linking slack user with okta user

--- example/example.go
+++ example/example.go
@@ -1,13 +1,64 @@
 package example
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/agext/uuid"
 	"github.com/go-chat-bot/bot"
+	"github.com/go-redis/redis"
 )
 
+type PocUser struct {
+	Groups  []string
+	Id      string
+	IsAuthd bool
+}
+
+func getPocUser(redisClient *redis.Client, Id string) *PocUser {
+	user, err := redisClient.Get(Id).Result()
+	if err != nil {
+		return &PocUser{
+			Groups:  []string{},
+			Id:      Id,
+			IsAuthd: false,
+		}
+	}
+	var deserializedUser PocUser
+	err = json.Unmarshal([]byte(user), &deserializedUser)
+	return &deserializedUser
+}
+
+func deauth(command *bot.Cmd) (msg string, err error) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:32768",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	user := getPocUser(redisClient, command.User.ID)
+	if user.IsAuthd {
+		redisClient.Del(command.User.ID)
+		msg = fmt.Sprintf("You are no longer authorized")
+		return
+	}
+	msg = fmt.Sprintf("You are not currently authorized. Please Authorize yourself in order to use this command")
+	return
+}
+
 func auth(command *bot.Cmd) (msg string, err error) {
-	msg = fmt.Sprintf("Whats up? %s", command.User.RealName)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:32768",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	user := getPocUser(redisClient, command.User.ID)
+	if user.IsAuthd {
+		msg = fmt.Sprintf("You are already authd, Your Id is %s, and your groups are %s", user.Id, user.Groups)
+		return
+	}
+	key := uuid.New().String()
+	redisClient.Set(key, command.User.ID, 0)
+	msg = fmt.Sprintf("Visit http://localhost:5050/link?key=%s", key)
 	return
 }
 
@@ -17,4 +68,9 @@ func init() {
 		"Authorizes slack user with okta backend",
 		"",
 		auth)
+	bot.RegisterCommand(
+		"deauth",
+		"Deauthorizes slack user",
+		"",
+		deauth)
 }
--- main.go
+++ main.go
@@ -1,45 +1,87 @@
 package main
 
 import (
-    "os"
+	"encoding/json"
+	"fmt"
+	"os"
 
-    "github.com/gin-gonic/gin"
-    "github.com/maximRnback/gin-oidc"
-    "net/url"
-    "github.com/gin-contrib/sessions"
+	"net/url"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"github.com/maximRnback/gin-oidc"
 
-    "github.com/go-chat-bot/bot/slack"
-    _ "github.com/mike-dunton/go-slack-bot-plugin-example/example"
+	"github.com/go-chat-bot/bot/slack"
+	_ "github.com/mike-dunton/go-slack-bot-plugin-example/example"
 )
 
-func main() {
-   go slack.Run(os.Getenv("dunton_dev_SLACK_TOKEN"))
+type PocUser struct {
+	Groups  []string
+	Id      string
+	IsAuthd bool
+}
 
-    r := gin.Default()
-    store := sessions.NewCookieStore([]byte("secret"))
-	  r.Use(sessions.Sessions("mysession", store))
-    issuerUrl, _ := url.Parse(os.Getenv("dunton_dev_OKTA_URL")+"/oauth2/default")
-    clientUrl, _ := url.Parse("http://localhost:5050/")
-    logoutUrl, _ := url.Parse(os.Getenv("dunton_dev_OKTA_URL"))
-    initParams := gin_oidc.InitParams{
-   		Router:       r,
-   		ClientId:     os.Getenv("dunton_dev_OKTA_CLIENT_ID"),
-   		ClientSecret: os.Getenv("dunton_dev_OKTA_CLIENT_SECRET"),
-   		Issuer:       *issuerUrl,
-   		ClientUrl:    *clientUrl,
-   		Scopes:       []string{"openid", "profile"},
-   		ErrorHandler: func(c *gin.Context) {
-        c.Redirect(503, "http://localhost:5050/error")
-   		},
-   		PostLogoutUrl: *logoutUrl,
-   	}
-    r.Use(gin_oidc.Init(initParams))
-  	r.GET("/ping", func(c *gin.Context) {
-      serverSession := sessions.Default(c)
-  		c.JSON(200, gin.H{
-  			"message": serverSession.Get("oidcClaims"),
-  		})
-  	})
-  	r.Run(":5050") // listen and serve on 0.0.0.0:8080
+func main() {
+	go slack.Run(os.Getenv("dunton_dev_SLACK_TOKEN"))
+
+	r := gin.Default()
+	store := sessions.NewCookieStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+	issuerUrl, _ := url.Parse(os.Getenv("dunton_dev_OKTA_URL") + "/oauth2/default")
+	clientUrl, _ := url.Parse("http://localhost:5050/")
+	logoutUrl, _ := url.Parse(os.Getenv("dunton_dev_OKTA_URL"))
+	initParams := gin_oidc.InitParams{
+		Router:       r,
+		ClientId:     os.Getenv("dunton_dev_OKTA_CLIENT_ID"),
+		ClientSecret: os.Getenv("dunton_dev_OKTA_CLIENT_SECRET"),
+		Issuer:       *issuerUrl,
+		ClientUrl:    *clientUrl,
+		Scopes:       []string{"openid", "profile"},
+		ErrorHandler: func(c *gin.Context) {
+			c.Redirect(503, "http://localhost:5050/error")
+		},
+		PostLogoutUrl: *logoutUrl,
+	}
+	r.Use(gin_oidc.Init(initParams))
+	r.GET("/link", func(c *gin.Context) {
+		keyString := c.Query("key")
+		redisClient := redis.NewClient(&redis.Options{
+			Addr:     "localhost:32768",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+		serverSession := sessions.Default(c)
+		userId, err := redisClient.Get(keyString).Result()
+		var claims map[string]interface{}
+		sessionClaims := serverSession.Get("oidcClaims")
+		json.Unmarshal([]byte(sessionClaims.(string)), &claims)
+		user := &PocUser{
+			IsAuthd: true,
+			Id:      userId,
+			Groups:  convertGroups(claims["groups"]),
+		}
+		serialized, err := json.Marshal(user)
+		err = redisClient.Set(userId, serialized, 0).Err()
+		if err != nil {
+			c.JSON(503, gin.H{
+				"message": err,
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"message": "Thank you for linking your slack user",
+		})
+
+	})
+	r.Run(":5050") // listen and serve on 0.0.0.0:8080
+}
+
+func convertGroups(groups interface{}) []string {
+	groupsSlice := groups.([]interface{})
+	s := make([]string, len(groupsSlice))
+	for i, v := range groupsSlice {
+		s[i] = fmt.Sprint(v)
+	}
+	return s
 }
